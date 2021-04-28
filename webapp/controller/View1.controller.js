325c2a9960de72983db6bc94037875058ada37b6
hankhank123 <woohank@gmail.com>
2021-04-28 15:02:18 +0800

    Initial commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,6 +4,10 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("aa1aa1.controller.View1", {
+<<<<<<< HEAD
+//123123
+=======
 
+>>>>>>> 8df0386354783072d576e30cdb4b2aeb74b7e47c
 	});
 });
